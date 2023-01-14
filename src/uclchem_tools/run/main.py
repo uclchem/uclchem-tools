b565cef935a64cf0f4f41ed2eb3738be8901dcb0
Gijs Vermariën <vermarien@strw.leidenuniv.nl>
2023-01-14 22:06:05 +0100

    Added initial scripts for uclchem pipeline

---
+++ src/uclchem_tools/run/main.py
@@ -0,0 +1,107 @@
+from argparse import ArgumentParser, RawTextHelpFormatter
+import subprocess
+import pathlib
+import yaml
+import logging
+import pandas as pd
+from importlib.metadata import version
+
+UCLCHEM_AVAIL = False
+try:
+    import uclchem
+
+    UCLCHEM_AVAIL = True
+except ModuleNotFoundError:
+    logging.info(
+        "We could not find UCLCHEM in this environment, to resolve this use a venv that has uclchem installed."
+    )
+
+
+def get_model(name):
+    return {
+        "cloud": uclchem.model.cloud,
+        "hot_core": uclchem.model.hot_core,
+        "collapse": uclchem.model.collapse,
+        "cshock": uclchem.model.cshock,
+        "jshock": uclchem.model.jshock,
+    }[name]
+
+
+def get_parser():
+    parser = ArgumentParser(description="info", formatter_class=RawTextHelpFormatter)
+    parser.add_argument(
+        "configpaths", nargs="+", help="A (list of) configuration file(s)"
+    )
+    parser.add_argument(
+        "--venvpath",
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    # See if we need to run using a venv or not. If so, recall ourselves and use the venv.
+    args = get_parser()
+
+    if args.venvpath:
+        logging.info(f"Running with virtual environment at {args.venvpath}")
+        subprocess.run(
+            f"{pathlib.Path(args.venvpath)/'bin/python'} {pathlib.Path(__file__).resolve()} {args.configpaths}",
+            shell=True,
+        )
+    elif UCLCHEM_AVAIL:
+        # Only import the rates module if AMUSE is there.
+        from rates import get_rates_of_change, rates_to_dfs
+
+        UCLCHEM_VERSION = version("uclchem")
+        logging.info(f"Running with UCLCHEM version {UCLCHEM_VERSION}")
+        for configpath in args.configpaths:
+            with open(configpath) as fh:
+                config = yaml.safe_load(fh)
+            # retrieve the name of the model, whether it is a key or a dict with additional arguments:
+            if isinstance(config["model"], str):
+                name = config["model"]
+                model_args = {}
+            elif isinstance(config["model"], dict):
+                name = str(list(config["model"].keys())[0])
+                model_args = config["model"]
+            # Obtain the model from uclchem
+            model = get_model(name)
+            # Run UCLCHEM
+            # model(
+            #     param_dict=config["param_dict"],
+            #     out_species=config["outspecies"],
+            #     **model_args,
+            # )
+            # CONVERT (csv -> hdf)
+            csvpath = config["param_dict"]["outputFile"]
+            datakey = pathlib.Path(csvpath).stem
+            if config["settings"]["hdf_save"] == True:
+                hdfpath = csvpath.with_suffix(".hdf5")
+            elif config["settings"]["hdf_save"]:
+                hdfpath = config["settings"]["hdf_save"]
+            else:
+                hdfpath = False
+            print(hdfpath)
+            with pd.HDFStore(hdfpath) as store:
+                df = uclchem.analysis.read_output_file(csvpath)
+                store.put(datakey + "/abundances", df)
+                print(config)
+                store.get_storer(datakey + "/abundances").attrs.metadata = config
+                # Add reactions and species from current UCLCHEM install
+                reactions = uclchem.utils.get_reaction_table()
+                species = uclchem.utils.get_species_table()
+                store.put(datakey + "/reactions", reactions)
+                store.put(datakey + "/species", species)
+                # POSTPROCESS UCLCHEM obtain the rates
+                rates_dict = get_rates_of_change(df, species, reactions)
+                for specie in list(species["Name"]):
+                    df_rates, df_production, df_destruction = rates_to_dfs(
+                        rates_dict, specie
+                    )
+                    store.put("{datakey}/{specie}/total_rates", df_rates)
+                    store.put("{datakey}/{specie}/production)", df_production)
+                    store.put("{datakey}/{specie}/df_destruction", df_destruction)
+    else:
+        logging.warning(
+            "No UCLCHEM could be found and no virtualenv with uclchem was specified. Not running any code."
+        )
---
+++ src/uclchem_tools/run/rates.py
@@ -0,0 +1,174 @@
+from tqdm import tqdm
+import numpy as np
+from copy import deepcopy
+import pandas as pd
+import uclchem
+from joblib import Parallel, delayed
+
+
+def key_reactions_to_dict(
+    time, total_production, total_destruction, key_reactions, key_changes
+):
+    """Prints key reactions to file
+
+    Args:
+        time (float): Simulation time at which analysis is performed
+        total_production (float): Total positive rate of change
+        total_destruction (float): Total negative rate of change
+        key_reactions (list): A list of all reactions that contribute to the total rate of change
+        key_changes (list): A list of rates of change contributing to total
+    """
+    return {
+        "time": float(time),
+        "total_production": float(total_production),
+        "total_destruction": float(total_destruction),
+        "key_production_reactions": {
+            str(reac): float(key_changes[k] / total_production)
+            for k, reac in enumerate(key_reactions)
+            if key_changes[k] > 0
+        },
+        "key_destruction_reactions": {
+            str(reac): float(key_changes[k] / total_destruction)
+            for k, reac in enumerate(key_reactions)
+            if key_changes[k] < 0
+        },
+    }
+
+
+def _get_rate_of_chance(result_df, reactions, species, species_name, rate_threshold):
+    fortran_reac_indxs = [
+        i + 1 for i, reaction in enumerate(reactions) if species_name in reaction
+    ]
+    reac_indxs = [i for i, reaction in enumerate(reactions) if species_name in reaction]
+    species_index = species.index(species_name) + 1  # fortran index of species
+    data = {}
+    if len(reac_indxs) <= 500 and species_name not in ["BULK", "SURFACE"]:
+        failed_species = []
+        for i, row in result_df.iterrows():
+            # recreate the parameter dictionary needed to get accurate rates
+            param_dict = uclchem.analysis._param_dict_from_output(row)
+            # get the rate of all reactions from UCLCHEM along with a few other necessary values
+            (rates, transfer, swap, bulk_layers,) = uclchem.analysis._get_species_rates(
+                param_dict, row[species], species_index, fortran_reac_indxs
+            )
+
+            # convert reaction rates to total rates of change, this needs manually updating when you add new reaction types!
+            change_reacs, changes = uclchem.analysis._get_rates_of_change(
+                rates,
+                reactions[reac_indxs],
+                species,
+                species_name,
+                row,
+                swap,
+                bulk_layers,
+            )
+
+            change_reacs = uclchem.analysis._format_reactions(change_reacs)
+
+            # This whole block adds the transfer of material from surface to bulk as surface grows (or vice versa)
+            # it's not a reaction in the network so won't get picked up any other way. We manually add it.
+            if species_name[0] == "@":
+                if transfer >= 0:
+                    change_reacs.append(
+                        f"#{species_name[1:]} + SURFACE_TRANSFER -> {species_name}"
+                    )
+                else:
+                    change_reacs.append(
+                        f"{species_name} + SURFACE_TRANSFER -> #{species_name[1:]}"
+                    )
+                changes = np.append(changes, transfer)
+            elif species_name[0] == "#":
+                if transfer >= 0:
+                    change_reacs.append(
+                        f"@{species_name[1:]} + SURFACE_TRANSFER -> {species_name}"
+                    )
+                else:
+                    change_reacs.append(
+                        f"{species_name} + SURFACE_TRANSFER -> @{species_name[1:]}"
+                    )
+                changes = np.append(changes, transfer)
+            # Then we remove the reactions that are not important enough to be printed by finding
+            # which of the top reactions we need to reach rate_threshold*total_rate
+            (
+                total_formation,
+                total_destruct,
+                key_reactions,
+                key_changes,
+            ) = uclchem.analysis._remove_slow_reactions(
+                changes, change_reacs, rate_threshold=rate_threshold
+            )
+            data[float(row["Time"])] = key_reactions_to_dict(
+                row["Time"],
+                total_formation,
+                total_destruct,
+                key_reactions,
+                key_changes,
+            )
+        # except:
+        #     if not species_name in failed_species:
+        #         print(f"for {species_name} something went wrong for step {i}.")
+        #         failed_species.append(species_name)
+        print(f"Just analyzed {species_name}")
+    else:
+        print(f"{species_name} contains more than 500 species, we are skipping it.")
+    return data
+
+
+def get_rates_of_change(result_df, species, reactions, rate_threshold=0.99):
+    """A function which loops over every time step in an output file and finds the rate of change of a species at that time due to each of the reactions it is involved in.
+    From this, the most important reactions are identified and printed to file. This can be used to understand the chemical reason behind a species' behaviour.
+
+    Args:
+        result_file (str): The path to the file containing the UCLCHEM output
+        rate_threshold (float,optional): Analysis output will contain the only the most efficient reactions that are responsible for rate_threshold of the total production and destruction rate. Defaults to 0.99.
+    """
+    species = list(species["Name"])
+    reactions = reactions[
+        [
+            "Reactant 1",
+            "Reactant 2",
+            "Reactant 3",
+            "Product 1",
+            "Product 2",
+            "Product 3",
+            "Product 4",
+        ]
+    ].to_numpy()
+    # all_analyses = {}
+    print(f"Found {len(species)} species, brace yourselves.")
+    # Get all the reaction rates with parallel worker:
+    rates = Parallel(n_jobs=100)(
+        delayed(_get_rate_of_chance)(
+            result_df, reactions, species, species_name, rate_threshold
+        )
+        for species_name in species
+    )
+    return {k: v for k, v in zip(species, rates)}
+
+    # new_name = results_file.replace(".dat", ".yaml")
+    # with open(new_name, "w") as outfile:
+    #     yaml.dump(all_analyses, outfile)
+
+
+def rates_to_dfs(data, specie):
+    if not data[specie]:
+        return pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
+    key_destruction_reactions = {}
+    key_production_reactions = {}
+    data_copy = deepcopy(data[specie])
+    for time_key in data_copy:
+        key_destruction_reactions[time_key] = data_copy[time_key].pop(
+            "key_destruction_reactions"
+        )
+        key_production_reactions[time_key] = data_copy[time_key].pop(
+            "key_production_reactions"
+        )
+    df = pd.DataFrame.from_dict(data_copy).transpose()
+    df.set_index("time", inplace=True)
+    df.index.set_names("Time", inplace=True)
+    df["total_destruction"] *= -1  # make destruction positive so we can plot it.
+    df_dest = pd.DataFrame.from_dict(key_destruction_reactions).transpose()
+    df_dest.index.set_names(["Time"], inplace=True)
+    df_prod = pd.DataFrame.from_dict(key_production_reactions).transpose()
+    df_prod.index.set_names(["Time"], inplace=True)
+    return df, df_prod, df_dest
